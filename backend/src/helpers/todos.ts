823db6f49170d7ad42bdc88c8a5dfb3640f04a76
Zulbil <alexkhang25@yahoo.fr>
2022-11-13 17:34:54 -0600

    create Todo business logic

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,4 +1,4 @@
-import { TodosAccess } from './todosAcess'
+import { TodoAccess } from './todosAcess'
 import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
@@ -8,3 +8,28 @@ import * as uuid from 'uuid'
 import * as createError from 'http-errors'
 
 // TODO: Implement businessLogic
+
+const todoAccess = new TodoAccess();
+
+export async function getTodos(): Promise<TodoItem[]> {
+    return todoAccess.getTodos();
+}
+
+export async function createTodo(
+    createTodoRequest: CreateTodoRequest,
+    jwtToken: string) : Promise<TodoItem> {
+
+        const itemId = uuid.v4();
+        //const userId = getUserId(jwtToken);
+        const userId = ''
+
+    return todoAccess.createTodo({
+        todoId: itemId,
+        userId: userId,
+        name: createTodoRequest.name,
+        dueDate: createTodoRequest.dueDate,
+        createdAt: new Date().toISOString(),
+        attachmentUrl: '',
+        done: true
+    })
+}
