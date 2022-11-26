ccc2b698823fad63b9c923bb5b654aeaaf46454b
Zulbil <alexkhang25@yahoo.fr>
2022-11-26 07:59:42 -0600

    add logger

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,17 +1,19 @@
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
+import { createLogger } from '../utils/logger';
 
 const bucketName: string = process.env.ATTACHMENT_S3_BUCKET
 const urlExpiration: number = 300;
+const logger: any = createLogger('attachmentUtil');
 
 const XAWS = AWSXRay.captureAWS(AWS)
 const s3: any = new XAWS.S3({ signatureVersion: 'v4' });
 
-
 export function createAttachmentPresignedUrl(imageId: string): string {
-    return s3.getSignedUrl('putObject', {
-      Bucket: bucketName,
-      Key: imageId,
-      Expires: urlExpiration
-    })
+  logger.info('create attachment presigned url');
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration
+  })
 }
