f33e521278e146016ac39c7a1257cd48711baaec
Zulbil <alexkhang25@yahoo.fr>
2022-11-26 08:00:07 -0600

    add updateAttachment method

--- backend/src/repositories/TodoRepository.ts
+++ backend/src/repositories/TodoRepository.ts
@@ -1,15 +1,17 @@
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate';
+import { createLogger } from "../utils/logger";
 
 export default class TodoRepository {
-    private todosTable: string           = process.env.TODOS_TABLE;
-    private indexName: string            = process.env.TODOS_CREATED_AT_INDEX; 
+    private todosTable: string           =  process.env.TODOS_TABLE;
+    private indexName: string            =  process.env.TODOS_CREATED_AT_INDEX; 
+    private logger: any               =  createLogger('TodoRepository'); 
 
     constructor(private docClient: DocumentClient) {}
 
     async getTodos(userId: string): Promise<TodoItem[]> {
-
+        this.logger.info('Performing query operation on todo table index');
         const result = await this.docClient.query({
           TableName: this.todosTable,
           IndexName: this.indexName,
@@ -23,8 +25,26 @@ export default class TodoRepository {
     
         return items as TodoItem[]
     }
+
+    async getTodo(userId: string, todoId: string): Promise<TodoItem> {
+      this.logger.info('Performing query operation on todo table index');
+      const result = await this.docClient.query({
+        TableName: this.todosTable,
+        IndexName: this.indexName,
+        KeyConditionExpression: 'userId = :userId, todoId = :todoId',
+        ExpressionAttributeValues: {
+          ':userId' : userId,
+          ':todoId' : todoId
+        }
+      }).promise();
+  
+      const item = result.Items[0]
+  
+      return item as TodoItem
+  }
     
     async createTodo(todoItem: TodoItem): Promise<TodoItem> {
+      this.logger.info('Performing put operation on todo table');
       await this.docClient.put({
         TableName: this.todosTable,
         Item: todoItem
@@ -34,6 +54,7 @@ export default class TodoRepository {
     }
     
     async updateTodo(todoId: string, userId: string, todoItem: Partial<TodoUpdate>): Promise<TodoUpdate> {
+      this.logger.info('Performing update operation on todo table');
       const updated = await this.docClient
           .update({
               TableName: this.todosTable,
@@ -58,8 +79,26 @@ export default class TodoRepository {
           .promise();
       return updated.Attributes as TodoUpdate;
     }
+
+    async updateTodoAttachment(todoId: string, userId: string, attachmentUrl: string): Promise<any> {
+      this.logger.info('Performing update attachment operation on todo table');
+      const updated = await this.docClient
+          .update({
+              TableName: this.todosTable,
+              Key: { userId, todoId },
+              UpdateExpression: "set #attachmentUrl = :attachmentUrl",
+              ExpressionAttributeNames: { "#attachmentUrl": "attachmentUrl" },
+              ExpressionAttributeValues: {
+                  ":attachmentUrl": attachmentUrl
+              },
+              ReturnValues: "ALL_NEW",
+          })
+          .promise();
+      return updated.Attributes as any;
+    }
   
     async deleteTodo(todoId: string, userId: string): Promise<any> {
+      this.logger.info('Performing delete operation on todo table');
         return await this.docClient.delete({
             TableName: this.todosTable,
             Key: { 
--- backend/src/services/TodoService.ts
+++ backend/src/services/TodoService.ts
@@ -4,34 +4,57 @@ import { CreateTodoRequest } from "../requests/CreateTodoRequest";
 import * as uuid from 'uuid';
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { TodoUpdate } from '../models/TodoUpdate';
-
+import { createLogger } from "../utils/logger";
 
 export default class TodoService {
+
+    private logger: any = createLogger('todoService')
     private bucketName: string = process.env.ATTACHMENT_S3_BUCKET;
 
     constructor() {}
 
     async getTodos(userId:string): Promise<TodoItem[]> {
+        this.logger.info('Get todos for connected user');
         return await todoRepository.getTodos(userId);
+    }
+
+    async getTodo(userId:string, todoId:string): Promise<TodoItem> {
+        this.logger.info('Get one todo for connected user');
+        return await todoRepository.getTodo(userId, todoId);
     }
     
     async createTodo(
         createTodoRequest: CreateTodoRequest,
         jwtToken: string) : Promise<TodoItem> {
-    
+        
+        try {
+
             const itemId            = uuid.v4();
             const userId            = jwtToken;
-            const attachmentUrl     = `https://${this.bucketName}.s3.amazonaws.com/${itemId}`;
-    
-        return todoRepository.createTodo({
-            todoId: itemId,
-            userId: userId,
-            name: createTodoRequest.name,
-            dueDate: createTodoRequest.dueDate,
-            createdAt: new Date().toISOString(),
-            attachmentUrl,
-            done: true
-        })
+
+            this.logger.info('Creating new todo');
+
+            const todoTocreate = {
+                todoId: itemId,
+                userId: userId,
+                name: createTodoRequest.name,
+                dueDate: createTodoRequest.dueDate,
+                createdAt: new Date().toISOString(),
+                attachmentUrl: '',
+                done: false
+            };
+
+            if (!todoTocreate.name) {
+                this.logger.info('Name property is invalid');
+                throw new Error('Please provide a valid name');
+            }
+        
+            return todoRepository.createTodo(todoTocreate);
+        } catch (error: any) {
+            this.logger.error(error.message);
+            throw new Error(error.message);
+        }
+        
     }
     
     async updateTodo(
@@ -40,16 +63,43 @@ export default class TodoService {
         updateTodoRequest: UpdateTodoRequest
         ) : Promise<TodoUpdate> {
         
+        try {
+            this.logger.info('Updating todo');
+
             const todoItemToUpdate : Partial<TodoUpdate> = {
                 name: updateTodoRequest.name,
                 dueDate: updateTodoRequest.dueDate,
                 done: updateTodoRequest.done
             };
-    
-        return await todoRepository.updateTodo(id, userId, todoItemToUpdate);
+
+            if (!todoItemToUpdate.name) {
+                this.logger.info('Name property is invalid');
+                throw new Error('Name property is invalid!');
+            }
+            return await todoRepository.updateTodo(id, userId, todoItemToUpdate);
+        } catch (error:any) {
+            this.logger.error(error.message);
+            throw new Error(error.message);
+        }
+    }
+
+    async updateTodoAttachmentUrl(
+        id: string,
+        userId: string
+        ) : Promise<TodoUpdate> {
+        
+        try {
+            this.logger.info('Updating todo');
+            const attachmentUrl = `https://${this.bucketName}.s3.amazonaws.com/${id}`;
+            return await todoRepository.updateTodoAttachment(id, userId, attachmentUrl);
+        } catch (error:any) {
+            this.logger.error(error.message);
+            throw new Error(error.message);
+        }
     }
     
     async deleteTodo(id: string, userId: string) {
+        this.logger.info('Deleting todo');
         await todoRepository.deleteTodo(id, userId);
     }
 }
